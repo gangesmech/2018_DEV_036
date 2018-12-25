1f7a6fdd23aab7724cd7e1f270fddfda1bbb18f4
ganges <ganges@gmail.com>
2018-12-25 12:05:06 +0530

    Implementation of Digital to Berlin Clock conversion

--- berlin-clock/src/main/java/com/bnpparibasfortis/berlinclock/converter/BerlinClockConverter.java
+++ berlin-clock/src/main/java/com/bnpparibasfortis/berlinclock/converter/BerlinClockConverter.java
@@ -1,5 +1,8 @@
 package com.bnpparibasfortis.berlinclock.converter;
 
+import java.time.LocalTime;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -8,6 +11,13 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class BerlinClockConverter {
 
+	private static final String LAMP_OFF = "O";
+	private static final String LAMP_ON_YELLOW = "Y";
+	private static final String LAMP_ON_RED = "R";
+	private static final int SINGLE_MINUTE_ROW_COUNT = 4;
+	private static final int FIVE_MINUTES_ROW_COUNT = 11;
+	private static final int HOUR_ROW_COUNT = 4;
+	
 	private static BerlinClockConverter berlinClockConverter;
 
 	private BerlinClockConverter(){}
@@ -20,30 +30,46 @@ public class BerlinClockConverter {
 	}
 	
 	public String getSingleMinuteRow(String digitalTime) {
-		return StringUtils.EMPTY;
+		LocalTime digitalLocalTime = LocalTime.parse(digitalTime);
+		String onLamps = IntStream.range(0, digitalLocalTime.getMinute() % 5).mapToObj(e -> LAMP_ON_YELLOW)
+				.collect(Collectors.joining(StringUtils.EMPTY));
+		return StringUtils.rightPad(onLamps, SINGLE_MINUTE_ROW_COUNT, LAMP_OFF);
 	}
 
 	public String getFiveMinutesRow(String digitalTime) {
-		return StringUtils.EMPTY;
+		LocalTime digitalLocalTime = LocalTime.parse(digitalTime);
+		String onLamps = IntStream.range(0, digitalLocalTime.getMinute() / 5)
+				.mapToObj(e -> (e + 1) % 3 == 0 ? LAMP_ON_RED : LAMP_ON_YELLOW)
+				.collect(Collectors.joining(StringUtils.EMPTY));
+		return StringUtils.rightPad(onLamps, FIVE_MINUTES_ROW_COUNT, LAMP_OFF);
 	}
 
 	public String getSingleHourRow(String digitalTime) {
-		return StringUtils.EMPTY;
+		LocalTime digitalLocalTime = LocalTime.parse(digitalTime);
+		String onLamps = IntStream.range(0, digitalLocalTime.getHour() % 5).mapToObj(e -> LAMP_ON_RED)
+				.collect(Collectors.joining(StringUtils.EMPTY));
+		return StringUtils.rightPad(onLamps, HOUR_ROW_COUNT, LAMP_OFF);
 	}
 
 	public String getFiveHoursRow(String digitalTime) {
-		return StringUtils.EMPTY;
+		LocalTime digitalLocalTime = LocalTime.parse(digitalTime);
+		String onLamps = IntStream.range(0, digitalLocalTime.getHour() / 5).mapToObj(e -> LAMP_ON_RED)
+				.collect(Collectors.joining(StringUtils.EMPTY));
+		return StringUtils.rightPad(onLamps, HOUR_ROW_COUNT, LAMP_OFF);
 	}
 
 	public String getSecondsLamp(String digitalTime) {
-		return StringUtils.EMPTY;
+		LocalTime digitalLocalTime = LocalTime.parse(digitalTime);
+		return digitalLocalTime.getSecond() % 2 == 0 ? LAMP_ON_YELLOW : LAMP_OFF;
 	}
 
 	public String getBerlinClockTime(String digitalTime) {
-		return StringUtils.EMPTY;
+		return getSecondsLamp(digitalTime) + getFiveHoursRow(digitalTime) + getSingleHourRow(digitalTime)
+				+ getFiveMinutesRow(digitalTime) + getSingleMinuteRow(digitalTime);
 	}
 
 	public String getDigitalClockTime(String berlinTime) {
 		return StringUtils.EMPTY;
 	}
+	
 }
